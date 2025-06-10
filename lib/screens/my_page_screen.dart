9a10771eb1ef9951e5cd4f9b356fe042a0aafe9a
kensei-yu <157794662+kensei-yu@users.noreply.github.com>
2025-06-10 17:28:32 +0900

    .ではなく：でした

--- lib/screens/my_page_screen.dart
+++ lib/screens/my_page_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package.cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'edit_profile_screen.dart';
 import 'welcome_screen.dart';
 
